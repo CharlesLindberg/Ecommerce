ee1eadf6fdaf873d7e219386cccd713f2577a454
CharlesLindberg <charleslindberg91@gmail.com>
2024-12-06 15:17:07 +0100

    HomePage styling

--- Frontend/src/Pages/HomePage.tsx
+++ Frontend/src/Pages/HomePage.tsx
@@ -1,7 +1,21 @@
+import styles from "./HomePage.module.css";
+
 const HomePage = () => {
   return (
-    <div>
+    <div className={styles.HomePage}>
       <h1>Welcome to the webshop</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quibusdam
+        nobis illo aspernatur quod molestias facilis cumque magni sequi,
+        pariatur veritatis doloremque, dolore obcaecati consequuntur?
+        Laudantium, inventore. Reprehenderit ratione earum illum autem fugit
+        cumque recusandae, perferendis placeat aliquid facere repellendus quae
+        itaque culpa dolore. Sapiente, quos veritatis facilis cumque ducimus,
+        quas nostrum accusantium sunt voluptates quisquam fugiat voluptatum
+        consequatur dolores deleniti, nisi earum dolore minus eaque! Distinctio
+        eos dolor voluptate. Deleniti assumenda sapiente omnis nobis explicabo
+        accusamus repellat hic unde!
+      </p>
     </div>
   );
 };
